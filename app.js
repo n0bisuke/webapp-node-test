b63efcacd057c0014235f904440d5272e2949586
のびすけ <rsksound@gmail.com>
2016-12-03 11:18:12 +0900

    edit app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000;
-const USERNAME = process.env.USERNAME || 'World';
+const USERNAME = process.env.WEBSITE_NODE_USER || 'World';
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
