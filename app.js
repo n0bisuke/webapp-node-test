7ce9c0c180d95582574ed2ffc23b4bd855b6c67e
のびすけ <rsksound@gmail.com>
2016-12-04 13:47:02 +0900

    deploy

--- app.js
+++ app.js
@@ -1,74 +1,41 @@
-'use strict'
+'use strict';
 
-const http = require('http');
-const https = require('https');
+const Hapi = require('hapi');
+const request = require('superagent');
 const crypto = require('crypto');
 
-const HOST = 'api.line.me'; 
-const REPLY_PATH = '/v2/bot/message/reply';//リプライ用
-const CH_SECRET = process.env.LINE_CHANNEL_SECRET || 'null'; //Channel Secretを指定
-const CH_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || 'null'; //Channel Access Tokenを指定
+const REPLY_URL = 'https://api.line.me/v2/bot/message/reply'; //リプライ用
+const CH_SECRET = process.env.LINE_CHANNEL_SECRET || require('./config').LINE_CHANNEL_SECRET; //Channel Secretを指定
+const CH_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || require('./config').LINE_CHANNEL_ACCESS_TOKEN; //Channel Access Tokenを指定
 const SIGNATURE = crypto.createHmac('sha256', CH_SECRET);
-const PORT = process.env.PORT || 3000;
-
-// http.createServer(function (request, response) {
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.end(`Hello World!! \n ${CH_SECRET} / ${CH_ACCESS_TOKEN}`);
-// }).listen(PORT);
- 
-// console.log(`Server running at http://127.0.0.1:${PORT}/`);
-
-const client = (replyToken, SendMessageObject) => {
-    const postDataStr = JSON.stringify({ replyToken: replyToken, messages: SendMessageObject });
-    const options = {
-        host: HOST,
-        port: 443,
-        path: REPLY_PATH,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'X-Line-Signature': SIGNATURE,
-            'Authorization': `Bearer ${CH_ACCESS_TOKEN}`
-        }
-    };
-
-    return new Promise((resolve, reject) => {
-        const req = https.request(options, (res) => {
-            let body = '';
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => {
-                body += chunk;
-            });
-            res.on('end', () => {
-                resolve(body);
-            });
+//
+let client = (replyToken, SendMessageObject) => {
+    request
+        .post(REPLY_URL)
+        .send({ replyToken: replyToken, messages: SendMessageObject })
+        .set('X-Line-Signature', SIGNATURE)
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${CH_ACCESS_TOKEN}`)
+        .end(function(err, res){
+            console.log(res.body);
         });
-        req.on('error', (e) => {
-            reject(e);
-        });
-        req.write(postDataStr);
-        req.end();
-    });
 };
 
-http.createServer((req, res) => {    
-    if(req.url !== '/' || req.method !== 'POST'){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(`konbanwa! \n ${process.env.WEBSITE_NODE_DEFAULT_VERSION} / ${CH_SECRET} / ${CH_ACCESS_TOKEN}`);
-    }
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 3000
+});
 
-    let body = '';
-    req.on('data', (chunk) => {
-        body += chunk;
-    });        
-    req.on('end', () => {
-    //   if(body == ''){
-    //     console.log(`bodyはから`);
-    //     return;
-    //   }
-      
-        let WebhookEventObject = JSON.parse(body).events[0]; 
-        //メッセージが送られて来た場合
+server.route({
+    method: 'POST',
+    path:'/callback',
+    handler: (request, reply) => {
+        console.log(request.payload.events);
+        console.log('-------\n');
+
+        let WebhookEventObject = request.payload.events[0];
+
         if(WebhookEventObject.type === 'message'){
             let SendMessageObject;
             if(WebhookEventObject.message.type === 'text'){
@@ -77,16 +44,15 @@ http.createServer((req, res) => {
                     text: WebhookEventObject.message.text
                 }];
             }
-            client(WebhookEventObject.replyToken, SendMessageObject)
-            .then((body)=>{
-                console.log(body);
-            },(e)=>{console.log(e)});
+
+            client(WebhookEventObject.replyToken, SendMessageObject);
         }
+        return reply('success').code(200);
+    }
+});
 
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('su');
-    });
-
-}).listen(PORT);
-
-console.log(`Server running at ${PORT}`);
+// Start the server
+server.start((err) => {
+    if (err) throw err;
+    console.log('Server running at:', server.info.uri);
+});
