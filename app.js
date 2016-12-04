f9ea41941e9e56e844e5d57825c710dfa249c3a7
のびすけ <rsksound@gmail.com>
2016-12-04 11:31:39 +0900

    deploy

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
+'use strict'
+
 const http = require('http');
 const https = require('https');
 const crypto = require('crypto');
 
 const HOST = 'api.line.me'; 
 const REPLY_PATH = '/v2/bot/message/reply';//リプライ用
-const CH_SECRET = "6da16a76c7390db72c8c42808f34e8eb" || 'null'; //Channel Secretを指定
-const CH_ACCESS_TOKEN = "s/DA31gIk+EQFVnbRtMion/rLciELBwurXTbkKyj7VmTnkzNfze0GZFBtZ8xCmXnnu/0gZ8objtdBIIRHs5lXYh8jsqETvkKu0u4tRYDTEiaUuHYbcKMvblU0ZQLgUyaptF38/WnJWbZ5dge/1F1qAdB04t89/1O/w1cDnyilFU=" || 'null'; //Channel Access Tokenを指定
+const CH_SECRET = process.env.LINE_CHANNEL_SECRET || 'null'; //Channel Secretを指定
+const CH_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || 'null'; //Channel Access Tokenを指定
 const SIGNATURE = crypto.createHmac('sha256', CH_SECRET);
 const PORT = process.env.PORT || 3000;
 
 // http.createServer(function (request, response) {
 //   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.end(`Hello World\n ${CH_SECRET} / ${CH_ACCESS_TOKEN}`);
+//   response.end(`Hello World!! \n ${CH_SECRET} / ${CH_ACCESS_TOKEN}`);
 // }).listen(PORT);
  
 // console.log(`Server running at http://127.0.0.1:${PORT}/`);
 
-/**
- * httpリクエスト部分
- */
-const client = (replyToken, SendMessageObject) => {    
-    let postDataStr = JSON.stringify({ replyToken: replyToken, messages: SendMessageObject });
-    let options = {
+const client = (replyToken, SendMessageObject) => {
+    const postDataStr = JSON.stringify({ replyToken: replyToken, messages: SendMessageObject });
+    const options = {
         host: HOST,
         port: 443,
         path: REPLY_PATH,
@@ -34,17 +33,16 @@ const client = (replyToken, SendMessageObject) => {
     };
 
     return new Promise((resolve, reject) => {
-        let req = https.request(options, (res) => {
-                    let body = '';
-                    res.setEncoding('utf8');
-                    res.on('data', (chunk) => {
-                        body += chunk;
-                    });
-                    res.on('end', () => {
-                        resolve(body);
-                    });
+        const req = https.request(options, (res) => {
+            let body = '';
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
+            res.on('end', () => {
+                resolve(body);
+            });
         });
-
         req.on('error', (e) => {
             reject(e);
         });
@@ -56,7 +54,7 @@ const client = (replyToken, SendMessageObject) => {
 http.createServer((req, res) => {    
     if(req.url !== '/' || req.method !== 'POST'){
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(`${process.env.WEBSITE_NODE_DEFAULT_VERSION} / ${CH_SECRET} / ${CH_ACCESS_TOKEN}`);
+        res.end(`konbanwa! \n ${process.env.WEBSITE_NODE_DEFAULT_VERSION} / ${CH_SECRET} / ${CH_ACCESS_TOKEN}`);
     }
 
     let body = '';
