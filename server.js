06d61b79b2e7864b761cae1516f6dbd205b8154a
のびすけ <rsksound@gmail.com>
2017-02-11 15:25:04 +0900

    色々カット

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {    
+    if(req.url !== '/' || req.method !== 'POST'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('hello');
+    }
+
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });        
+    req.on('end', () => {
+        if(body === ''){
+          console.log('bodyが空です。');
+          return;
+        }
+
+        let WebhookEventObject = JSON.parse(body).events[0];  
+        if(WebhookEventObject.type === 'beacon'){
+            console.log('beacon発見');
+            console.log(WebhookEventObject);
+            return;
+        }  
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('success');
+    });
+
+}).listen(PORT);
+
+console.log(`Server running at ${PORT}`);
